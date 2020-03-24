30cf77c909df08dfae6f2b4ce3986506ff9664e7
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-24 07:41:16 +0000

    Add: フラッシュの練習

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -15,4 +15,13 @@ class LessonController < ApplicationController
   def step4
     render plain: 'step4に移動しました。'
   end
+
+  def step5
+    flash[:notice] = 'step6に移動します。'
+    redirect_to action: 'step6'
+  end
+
+  def step6
+    render plain: flash[:notice]
+  end
 end
