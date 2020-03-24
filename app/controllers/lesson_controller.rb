3944533f606105b70aad9bbb98a94d7d0358b682
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-24 07:29:59 +0000

    Add: step3,step4アクションを追加

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -7,4 +7,12 @@ class LessonController < ApplicationController
     render plain: request.env["HTTP_USER_AGENT"] #ブラウザの種類
     # render plain: request.remote_ip #IPアドレス
   end
+
+  def step3
+    redirect_to action: 'step4'
+  end
+
+  def step4
+    render plain: 'step4に移動しました。'
+  end
 end
