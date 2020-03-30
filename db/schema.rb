1802dc5fa30223847e48efa4af9a435fc7028e46
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-30 07:12:09 +0000

    Add: seeファイルを作成し、ユーザーを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_042120) do
+ActiveRecord::Schema.define(version: 2020_03_26_044845) do
 
   create_table "members", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_03_26_042120) do
     t.boolean "administrator", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
