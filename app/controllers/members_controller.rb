d54acf833f197a59c41176eb44e26be4554f5f97
KUGA16 <gachan_1216@yahoo.co.jp>
2020-03-30 10:03:07 +0000

    Add: destroyアクションを定義

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -41,5 +41,8 @@ class MembersController < ApplicationController
   end
 
   def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました。"
   end
 end
